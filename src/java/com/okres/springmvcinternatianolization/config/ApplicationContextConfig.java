7a848efe47515bdb90db2b9a608feb69f02ea990
SifuUA <alekseykres@gmail.com>
2017-07-30 13:58:50 +0300

    add congig classes

--- src/java/com/okres/springmvcinternatianolization/config/ApplicationContextConfig.java
+++ src/java/com/okres/springmvcinternatianolization/config/ApplicationContextConfig.java
@@ -1,7 +1,48 @@
 package com.okres.springmvcinternatianolization.config;
 
+import com.sun.corba.se.spi.resolver.LocalResolver;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 /**
  * Created by Alex on 30.07.2017.
  */
+
+@Configuration
+
+@ComponentScan("com.okres.springmvcinternatianolization.*")
 public class ApplicationContextConfig {
+
+    @Bean(name = "viewResolver")
+    public InternalResourceViewResolver getViewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+
+    @Bean(name = "messageSource")
+    public MessageSource getMessageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean(name = "localResolver")
+    public LocalResolver getLocalResolver(){
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setCookieDomain("myAppLocalCookie");
+
+        //60 minutes
+        resolver.setCookieMaxAge(60*60);
+        return (LocalResolver) resolver;
+    }
 }
--- src/java/com/okres/springmvcinternatianolization/config/WebMvcConfig.java
+++ src/java/com/okres/springmvcinternatianolization/config/WebMvcConfig.java
@@ -1,7 +1,30 @@
 package com.okres.springmvcinternatianolization.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
 /**
  * Created by Alex on 30.07.2017.
  */
-public class WebMvcConfig {
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter{
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //default
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+
+        registry.addInterceptor(localeChangeInterceptor).addPathPatterns("/*");
+    }
 }
